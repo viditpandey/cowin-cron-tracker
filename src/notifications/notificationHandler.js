658a020851e5ff905090c02dec30459aeb33b74c
Vidit Pandey <pandey.avi8@gmail.com>
2021-05-12 13:41:17 +0530

    pick email config from env vars

--- src/notifications/notificationHandler.js
+++ src/notifications/notificationHandler.js
@@ -3,15 +3,27 @@ const nodemailer = require("nodemailer")
 const NotificationHandler = {
     transporter: null,
     init: async () => {
-        NotificationHandler.transporter = nodemailer.createTransport({
-            service: 'gmail',
-            auth: {
-              user: process.env.AUTH_USERNAME_GMAIL || 'viditpandeytest@gmail.com',
-              pass: process.env.AUTH_PASS_GMAIL || 'ViditPandeyTest'
-            },
-          })
+        if (!process.env.AUTH_USERNAME_GMAIL) {
+            console.log('[NotificationHandler.init] failed since no configured email found.')
+            return
+        } else if (!process.env.AUTH_PASS_GMAIL) {
+            console.log('[NotificationHandler.init] failed since no configured email pwd found.')
+            return
+        } else {
+            NotificationHandler.transporter = nodemailer.createTransport({
+                service: 'gmail',
+                auth: {
+                  user: process.env.AUTH_USERNAME_GMAIL,
+                  pass: process.env.AUTH_PASS_GMAIL
+                },
+              })
+        }
     },
     sendMail: async (to, subject, data) => {
+        if (!NotificationHandler.transporter) {
+            console.log('[NotificationHandler.sendMail] mail transporter not configured, value is null.')
+            return
+        }
         let info = await NotificationHandler.transporter.sendMail({
             from: 'Vidit Pandey via cowin-covid-tracker',
             to: 'vidit8794@gmail.com',
