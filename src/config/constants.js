041f6a26f54ffe3df18d6b1fc24143563a90ba41
Vidit Pandey <pandey.avi8@gmail.com>
2021-05-12 09:28:49 +0530

    emails added

--- src/config/constants.js
+++ src/config/constants.js
@@ -10,7 +10,7 @@ const whatTo = [
         district_id: 646,
         district_name: "Ayodhya",
         min_age_limit: 18,
-        receivers: "pandey.avi8@gmail.com,shwetavalunj@gmail.com,alokverma816@gmail.com,vinodpandey8794@gmail.com,"
+        receivers: "pandey.avi8@gmail.com,shwetavalunj@gmail.com,alokverma816@gmail.com,yadavshubham20091994@gmail.com,"
     },
     {
         district_id: 646,
