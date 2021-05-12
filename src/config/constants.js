bbc87a9f45fb71b534fb2d5a54fe81d9dcb5c2c8
Vidit Pandey <pandey.avi8@gmail.com>
2021-05-12 13:55:04 +0530

    added for Kerala

--- src/config/constants.js
+++ src/config/constants.js
@@ -8,38 +8,38 @@ const pollingCronInterval = '*/15 2-10 * * *'
 const whatTo = [
     {
         district_id: 646,
-        district_name: "Ayodhya",
+        district_name: "Ayodhya (UP)",
         min_age_limit: 18,
         receivers: "pandey.avi8@gmail.com,alokverma816@gmail.com,yadavshubham20091994@gmail.com,"
     },
     // {
     //     district_id: 646,
-    //     district_name: "Ayodhya",
+    //     district_name: "Ayodhya (UP)",
     //     min_age_limit: 45,
     //     // receivers: "pandey.avi8@gmail.com,shwetavalunj@gmail.com,vinodpandey8794@gmail.com"
     //     receivers: "pandey.avi8@gmail.com,alokverma816@gmail.com"
     // },
     {
         district_id: 670,
-        district_name: "Lucknow",
+        district_name: "Lucknow(UP)",
         min_age_limit: 18,
         receivers: "pandey.avi8@gmail.com,siddhartha.Sehgal94@gmail.com,nmt.1615@gmail.com,aparajit35@gmail.com,chatterjee.raju@rediffmail.com"
     },
     {
         district_id: 363,
-        district_name: "Pune",
+        district_name: "Pune (Maharashtra)",
         min_age_limit: 18,
         receivers: "pandey.avi8@gmail.com,shwetavalunj@gmail.com,arnavmo@gmail.com"
     },
     {
         district_id: 624,
-        district_name: "Prayagraj",
+        district_name: "Prayagraj (UP)",
         min_age_limit: 18,
         receivers: "pandey.avi8@gmail.com"
     },
     {
         district_id: 697,
-        district_name: "Dehradun",
+        district_name: "Dehradun (UK)",
         min_age_limit: 18,
         receivers: "pandey.avi8@gmail.com,yatharthdeoly@gmail.com"
     },
@@ -55,12 +55,12 @@ const whatTo = [
         min_age_limit: 18,
         receivers: "pandey.avi8@gmail.com,divyakumari.1012@gmail.com"
     },
-    // {
-    //     district_id: 150,
-    //     district_name: "South West Delhi",
-    //     min_age_limit: 18,
-    //     receivers: "pandey.avi8@gmail.com,divyakumari.1012@gmail.com"
-    // },
+    {
+        district_id: 307,
+        district_name: "Ernakulam (Kerala)",
+        min_age_limit: 18,
+        receivers: "pandey.avi8@gmail.com,alpanakabra@gmail.com"
+    },
 ]
 
 const whereTo = {
