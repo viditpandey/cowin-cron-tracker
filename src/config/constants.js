5ea948a56a62e76ca33546164e368ab47e785a2d
Vidit Pandey <pandey.avi8@gmail.com>
2021-05-16 12:17:38 +0530

    turn off jobs for LKO, Dehradun

--- src/config/constants.js
+++ src/config/constants.js
@@ -10,7 +10,7 @@ const whatTo = [
         district_id: 646,
         district_name: "Ayodhya (UP)",
         min_age_limit: 18,
-        receivers: "pandey.avi8@gmail.com,alokverma816@gmail.com,yadavshubham20091994@gmail.com,"
+        receivers: "pandey.avi8@gmail.com,yadavshubham20091994@gmail.com,"
     },
     // {
     //     district_id: 646,
@@ -19,12 +19,12 @@ const whatTo = [
     //     // receivers: "pandey.avi8@gmail.com,shwetavalunj@gmail.com,vinodpandey8794@gmail.com,alokverma816@gmail.com"
     //     receivers: "pandey.avi8@gmail.com"
     // },
-    {
-        district_id: 670,
-        district_name: "Lucknow(UP)",
-        min_age_limit: 18,
-        receivers: "pandey.avi8@gmail.com,nmt.1615@gmail.com,aparajit35@gmail.com,chatterjee.raju@rediffmail.com"
-    },
+    // {
+    //     district_id: 670,
+    //     district_name: "Lucknow(UP)",
+    //     min_age_limit: 18,
+    //     receivers: "pandey.avi8@gmail.com,nmt.1615@gmail.com,aparajit35@gmail.com,chatterjee.raju@rediffmail.com"
+    // },
     {
         district_id: 363,
         district_name: "Pune (Maharashtra)",
@@ -37,12 +37,12 @@ const whatTo = [
         min_age_limit: 18,
         receivers: "pandey.avi8@gmail.com,shalabhms13@gmail.com"
     },
-    {
-        district_id: 697,
-        district_name: "Dehradun (UK)",
-        min_age_limit: 18,
-        receivers: "pandey.avi8@gmail.com,yatharthdeoly@gmail.com"
-    },
+    // {
+    //     district_id: 697,
+    //     district_name: "Dehradun (UK)",
+    //     min_age_limit: 18,
+    //     receivers: "pandey.avi8@gmail.com,yatharthdeoly@gmail.com"
+    // },
     {
         district_id: 179,
         district_name: "Anand (Gujarat)",
