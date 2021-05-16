33bd3acf8a07ece7c32756b524e9587bf9531d07
Vidit Pandey <pandey.avi8@gmail.com>
2021-05-16 15:36:54 +0530

    config changes

--- src/config/constants.js
+++ src/config/constants.js
@@ -6,12 +6,12 @@ const checkNewJobsInterval = '* * * * * *'
 const pollingCronInterval = '*/15 1-18 * * *'
 
 const whatTo = [
-    {
-        district_id: 646,
-        district_name: "Ayodhya (UP)",
-        min_age_limit: 18,
-        receivers: "pandey.avi8@gmail.com,yadavshubham20091994@gmail.com,"
-    },
+    // {
+    //     district_id: 646,
+    //     district_name: "Ayodhya (UP)",
+    //     min_age_limit: 18,
+    //     receivers: "pandey.avi8@gmail.com,yadavshubham20091994@gmail.com,"
+    // },
     // {
     //     district_id: 646,
     //     district_name: "Ayodhya (UP)",
